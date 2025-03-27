fc68f5dd6f76675d23e41158a4453310acaa43a8
dimpal001 <dimpaldas518@gmail.com>
2025-03-27 14:21:50 +0530

    add multiple database

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,36 @@
-require('dotenv').config();
-const mysql = require('mysql2/promise'); 
+require('dotenv').config()
+const mysql = require('mysql2/promise')
 
-const db = mysql.createPool({ 
+const db1 = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: process.env.DB1_NAME,
   waitForConnections: true,
-  connectionLimit: 10, 
-  queueLimit: 0
-});
+  connectionLimit: 10,
+  queueLimit: 0,
+})
 
-db.getConnection()
-  .then(() => console.log('Connected to MySQL database.'))
-  .catch((err) => console.error('Database connection failed:', err));
+const db2 = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB2_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
 
-module.exports = db;
+db1
+  .getConnection()
+  .then(() => console.log('Connected to MySQL database 1.'))
+  .catch((err) => console.error('Database connection failed:', err))
+
+db2
+  .getConnection()
+  .then(() => console.log('Connected to MySQL database 2.'))
+  .catch((err) => console.error('Database connection failed:', err))
+
+module.exports = { db1, db2 }
